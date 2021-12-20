0c7c23b9fe3da198200331418d240b309484d14c
Will Phillips <42WPhillips42@gmail.com>
2021-12-20 16:45:12 -0600

    form about halfway built

---
+++ front-end2/src/components/Organizer.js
@@ -0,0 +1,117 @@
+import react from "react";
+import TimePicker from "react-time-picker";
+
+//import styled from 'styled-components';
+
+const Organizer = (props) => {
+    const { values, submit, change, disabled, errors } = props;
+
+    const onSubmit = evt => {
+        evt.preventDefault();
+        submit(); 
+    }
+    const onChange = evt => {
+        console.log(evt.target.checked, evt.target.type);
+        const { name, value, checked, type } = evt.target;
+        const valueToUse = type === 'checkbox' ? checked : value;
+        change(name, valueToUse);
+    }
+
+    const today = new Date();
+
+    return (
+        <div>
+            <h2>Create a potluck!</h2>
+            <p>Submit details about yourself and the event you're organizing in order to share your potluck!</p>
+            <div id="event-organizer">'
+                <label>Organizer Name 
+                    <input 
+                        value={values.organizerName}
+                        onChange={onChange}
+                        name='organizerName'
+                        type='text'
+                        placeholder='Enter your name here'
+                        maxLength='25'
+                    />
+            </label>
+
+                <label>Organizer E-mail Address
+                    <input
+                        value={values.organizerEmail}
+                        onChange={onChange}
+                        name='organizerEmail'
+                        type='email'
+                        placeholder='Enter your email here'
+                        />
+                </label>
+
+                <label>Organizer Phone Number
+                    <input 
+                        value={values.organizerPhone}
+                        onChange={onChange}
+                        name='organizerPhone'
+                        type='tel'
+                        placeholder="Format: 123-456-7890"
+                        pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                        maxLength='12'
+                    />
+                </label>
+
+                <label>Organizer access password
+                    <p>Use this password to access and edit event</p>
+                    <input 
+                        value={values.accessPassword}
+                        onChange={onChange}
+                        name='accessPassword'
+                        type="password"
+                        placeholder="Remember this password!"
+                        maxLength="15"
+                    />
+                </label>
+            </div>
+
+            <div id="event-details">
+                <p>Provide some details about your event so you can share it with guests.</p>
+                <label>Potluck Name
+                    <input
+                        value={values.potluckName}
+                        onChange={onChange}
+                        name='potluckName'
+                        type='text'
+                        maxLength='15'
+                        placeholder="Enter Potluck Name"
+                    />
+                </label>
+
+                <label>Date of Potluck
+                    <input
+                        value={values.potluckDate}
+                        type="date"
+                        name="potluckDate"
+                        placeholder={today}
+                    />
+                </label>
+                    
+                <label>Approximate Potluck Time
+                {/* https://www.npmjs.com/package/react-time-picker to style this element */}
+                    <TimePicker
+                        onChange={onChange}
+                        value={values.potluckTime}
+                    />
+                </label>
+
+                <label>Potluck Location
+                    <input 
+                        type="text"
+                        value={values.potluckLocation}
+                        name="potluckLocation"
+                        maxLength="30"
+                        placeholder="Enter Potluck Location"
+                    />
+                </label>
+
+            </div>
+        </div>
+    )
+
+}
