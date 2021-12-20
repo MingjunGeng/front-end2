a77c43ac46b5f6277fbfdcbf028893dfd055e0ad
Tafiqul <demianviotto@gmail.com>
2021-12-20 18:09:51 -0500

    created home component. Working on styling it

--- front-end2/src/App.js
+++ front-end2/src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route, Link } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
+
 
 function App() {
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className='navpage'>
+        <nav>
+          <Link to='/'>Home</Link>
+          <Route exact path='/' component={Home} />
+        </nav>
       </header>
     </div>
   );
---
+++ front-end2/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from 'styled-components';
+
+
+function Home(){
+    return (
+      <div className='container'>
+          <h1>POTLUCK PALOOZA</h1>
+      </div>
+    )
+  }
+
+  export default Home;
