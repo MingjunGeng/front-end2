cf689665efe3ff5c5685f671570d7919f9cca21a
sooof <sooof@me.com>
2022-01-01 23:34:11 -0800

    AddUserToPotlucks test

---
+++ front-end2/src/components/AddUserToPotlucks.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const AddUserToPotlucks = () => {
+
+    return (
+        <div>
+            <h1>AddUserToPotlucks Test</h1>
+        </div>
+    )
+}
+
+export default AddUserToPotlucks;
